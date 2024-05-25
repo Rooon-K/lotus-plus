240c80c296dcf5a229eb796861bf17907f909d6f
rooon-k <2251944982@qq.com>
2024-05-25 23:55:48 +0800

    feat: 新增Link组件

--- packages/components/Link/Link.vue
+++ packages/components/Link/Link.vue
@@ -1,9 +1,60 @@
 <script setup lang="ts">
+import type { LinkEmits, LinkProps } from "./types";
+import { LIcon } from "../Icon";
+import { computed } from "vue";
+
 defineOptions({
   name: "LLink"
-})
+});
+const slots = defineSlots();
+const props = withDefaults(defineProps<LinkProps>(), {
+  status: "normal",
+  target: "_self"
+});
+const emits = defineEmits<LinkEmits>();
+
+const handleClick = (e: MouseEvent) => {
+  if (props.disabled || props.loading) {
+    e.preventDefault();
+    return;
+  }
+  emits("click", e);
+};
+const icon = computed(() => {
+  return props.loading ? "spinner" : props.icon;
+});
+const loading = computed(() => props.loading);
+const iconStyle = computed(() => ({
+  marginRight: slots.default ? "6px" : "0px"
+}));
 </script>
 
 <template>
-  <a href=""></a>
+  <a
+    :href="props.href ?? void 0"
+    class="lotus-link"
+    :class="{
+      [`lotus-link--${props.status}`]: props.status,
+      'lotus-link--disabled': props.disabled,
+      'lotus-link--loading': props.loading
+    }"
+    @click="handleClick"
+    :target="props.target"
+  >
+    <template v-if="loading">
+      <slot name="loading">
+        <l-icon
+          class="loading-icon"
+          icon="spinner"
+          :style="iconStyle"
+          size="1x"
+          spin
+        />
+      </slot>
+    </template>
+    <l-icon v-if="icon && !loading" size="1x" :icon="icon" :style="iconStyle" />
+    <span v-if="$slots.default" class="lotus-link__text">
+      <slot></slot>
+    </span>
+  </a>
 </template>
--- packages/components/Link/types.ts
+++ packages/components/Link/types.ts
@@ -1,15 +1,16 @@
 import type { Component } from "vue";
 
-export type LinkType = "primary" | "success" | "warning" | "danger" | "info" | "default";
+export type LinkStatus = "primary" | "success" | "warning" | "danger" | "normal";
 export type LinkTarget = "_blank" | "_self" | "_parent" | "_top";
 
 export interface LinkProps {
-  type: LinkType;
-  underline: boolean;
-  disabled: boolean;
-  href: string;
-  target: LinkTarget;
-  icon: string | Component;
+  status?: LinkStatus;
+  underline?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  href?: string;
+  target?: LinkTarget;
+  icon?: string | Component;
 }
 
 export interface LinkEmits {
