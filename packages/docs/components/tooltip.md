0a84b9403bc9302cca8090a0c8111b47b3418d7b
rooon-k <2251944982@qq.com>
2024-06-13 00:01:42 +0800

    docs: 添加位置修饰符的文档描述

--- packages/docs/components/tooltip.md
+++ packages/docs/components/tooltip.md
@@ -1,7 +1,15 @@
 # Tooltip 文字提示
 
 ## 基础用法
+通过使用`v-tooltip`指令, 给元素添加文字提示。
 
 ::: preview
 demo-preview=../demo/tooltip/Basic.vue
+:::
+
+## 修饰符用法
+通过位置修饰符`position`来控制文字提示的位置。
+
+::: preview
+demo-preview=../demo/tooltip/Modifiers.vue
 :::
---
+++ packages/docs/demo/tooltip/Modifiers.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <l-row justify="center">
+      <l-button
+        status="primary"
+        v-tooltip.top="'This is a two-line tooltip content.This is a two-line tooltip content.'"
+        >Top</l-button
+      >
+    </l-row>
+    <l-row justify="space-between" :span="24">
+      <l-col :span="4">
+        <l-button
+          status="primary"
+          v-tooltip.left="'This is a two-line tooltip content.This is a two-line tooltip content.'"
+          >Left</l-button
+        >
+      </l-col>
+      <l-col :span="4">
+        <l-button
+          status="primary"
+          v-tooltip.right="'This is a two-line tooltip content.This is a two-line tooltip content.'"
+          >Right</l-button
+        >
+      </l-col>
+    </l-row>
+    <l-row justify="center">
+      <l-button
+        status="primary"
+        v-tooltip.bottom="'This is a two-line tooltip content.This is a two-line tooltip content.'"
+        >Bottom</l-button
+      >
+    </l-row>
+  </div>
+</template>
